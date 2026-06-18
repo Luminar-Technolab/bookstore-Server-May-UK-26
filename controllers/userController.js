f8d9adc1fb606c77ce9dee8432ce634cb248b25f
soniagkalliath <soniageorge111@gmail.com>
2026-06-18 15:49:05 +0530

    register api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,24 @@
 const users = require('../models/userModel')
+const bcrypt = require('bcrypt')
 
 // register
 exports.register = async (req,res)=>{
     console.log("Inside register controller");
-    console.log(req.body);
-    res.status(200).json("Register request recieved")
+    const {username,email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            res.status(409).json("User already exists.. Please Login!!!")
+        }else{
+            let encryptPassword = await bcrypt.hash(password,10)
+            //add user to db
+            const newUser = await users.create({username,email,password:encryptPassword})
+            res.status(201).json(newUser)
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err)
+    }
 }
 
 // login
