b851f04449f0b7c35f2e457471be6eba56be47a9
soniagkalliath <soniageorge111@gmail.com>
2026-06-19 15:37:37 +0530

    Login & Goole Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const users = require('../models/userModel')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 // register
 exports.register = async (req,res)=>{
@@ -22,7 +23,48 @@ exports.register = async (req,res)=>{
 }
 
 // login
+exports.login = async (req,res)=>{
+    console.log("Inside login controller");
+    const {email,password} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            const isPasswordMatch = await bcrypt.compare(password,existingUser.password)
+            if(isPasswordMatch){
+                //generate token 
+                const token = jwt.sign({userMail:email,role:existingUser.role},process.env.JWT_SECRET)
+                res.status(200).json({user:existingUser,token})
+            }else{
+                res.status(409).json("Invalid Email / Password!!!")
+            }
+        }else{
+            res.status(409).json("Invalid Email!!! Please Register to access Bookstore")
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err)
+    }
+}
 
 // google login
-
+exports.googleLogin = async (req,res)=>{
+    console.log("Inside googleLogin controller");
+    const {email,password,username,picture} = req.body
+    try{
+        const existingUser = await users.findOne({email})
+        if(existingUser){
+            const token = jwt.sign({userMail:existingUser.email,role:existingUser.role},process.env.JWT_SECRET)
+            res.status(200).json({user:existingUser,token})
+        }else{
+            let encryptPassword = await bcrypt.hash(password,10)
+            //add user to db
+            const newUser = await users.create({username,email,password:encryptPassword,picture})
+            const token = jwt.sign({userMail:newUser.email,role:newUser.role},process.env.JWT_SECRET)
+            res.status(200).json({user:newUser,token})
+        }
+    }catch(err){
+        console.log(err);
+        res.status(500).json(err)
+    }
+}
 // user updation
--- routes/bookstoreRoutes.js
+++ routes/bookstoreRoutes.js
@@ -6,6 +6,10 @@ const router = new express.Router()
 //global routes
 //register
 router.post('/register',userController.register)
+//login
+router.post('/login',userController.login)
+//google login
+router.post('/google-login',userController.googleLogin)
 
 //AUTHORISED ROUTES
 
